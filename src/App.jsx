5ab811135a0b5b697f7575afbd67560f714b5140
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-04-14 17:56:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './index.css'; 
+ import { FaBeer } from 'react-icons/fa';
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex items-center justify-center h-screen bg-gray-800 text-white
+      text-2xl font-bold'>
+     Welcome <FaBeer/>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
