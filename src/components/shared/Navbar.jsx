0eff5678df0c3d6ab4127695b20612ba1a2684b4
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-05-09 23:25:58 +0530

    Update Navbar.jsx

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -10,7 +10,8 @@ const Navbar = () => {
 
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
-    const { cart } = useSelector(state => state.carts)
+    const { cart } = useSelector(state => state.carts);
+    const { user } = useSelector(state => state.auth);
 
     return(
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -61,6 +62,11 @@ const Navbar = () => {
                     </Link>
                 </li>
 
+               {(user && user.id) ? (
+                      <li className="font-[500] transition-all duration-150">
+                      <p>Welcome</p>
+                      </li>
+               ) : (
                 <li className="font-[500] transition-all duration-150">
                     <Link className="flex items-center space-x-2 px-4 py-[6px]
                     bg-gradient-to-r from-purple-600 to-red-500
@@ -71,6 +77,8 @@ const Navbar = () => {
                       <span>Login</span>
                     </Link>
                 </li>
+               )
+                }
               </ul>
               <button onClick={() => setNavbarOpen(!navbarOpen)}
                 className="sm:hidden flex items-center sm:mt-0 mt-2"
