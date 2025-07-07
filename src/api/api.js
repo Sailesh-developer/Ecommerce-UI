4f2ecb2aeba670bb1004792fd7f633ed9511287c
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-07-07 23:22:56 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -8,16 +8,16 @@ export const api = axios.create({
     );
 
 
-api.interceptors.request.use(
-      (config) => {
-            const auth = JSON.parse(localStorage.getItem("auth"));
-            const token = auth?.jwtToken;
+// api.interceptors.request.use(
+//       (config) => {
+//             const auth = JSON.parse(localStorage.getItem("auth"));
+//             const token = auth?.jwtToken;
 
-            if (token) {
-                  config.headers.Authorization = `Bearer ${token}`;
-            }
+//             if (token) {
+//                   config.headers.Authorization = `Bearer ${token}`;
+//             }
 
-            return config;
-      },
-      (error) => Promise.reject(error)
-);    
+//             return config;
+//       },
+//       (error) => Promise.reject(error)
+// );    
